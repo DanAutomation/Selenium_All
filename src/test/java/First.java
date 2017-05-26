ae7dbed740aa14948c10aa9c612ac896275ae09e
DanAutomation <kasta-laga@yandex.ru>
2017-05-26 12:19:15 +0300

    Second lesson

--- src/test/java/First.java
+++ src/test/java/First.java
@@ -1,9 +1,13 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
 
 /**
  * Created by QAEngineer on 25.05.2017.
@@ -16,12 +20,21 @@ public class First {
     @Before
     public void start(){
         driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         wait = new WebDriverWait(driver, 10);
     }
 
     @Test
-    public void firstTest(){
-        driver.get("http://www.google.com");
+    public void firstTest() throws InterruptedException {
+        driver.get("http://localhost/litecart/admin/login.php");
+        Thread.sleep(2000);
+
+        driver.findElement(By.name("username")).sendKeys("admin");
+        Thread.sleep(2000);
+        driver.findElement(By.name("password")).sendKeys("admin");
+        Thread.sleep(2000);
+        driver.findElement(By.name("login")).click();
+
     }
 
     @After
@@ -29,4 +42,5 @@ public class First {
         driver.quit();
         driver = null;
     }
+
 }
