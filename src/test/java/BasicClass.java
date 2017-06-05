d7cceee94750aa78c51da7ed52e05693a5aa75e9
DanAutomation <kasta-laga@yandex.ru>
2017-06-05 21:58:24 +0300

    update_05062017

--- src/test/java/BasicClass.java
+++ src/test/java/BasicClass.java
@@ -4,20 +4,17 @@ import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
 /**
- * Created by QAEngineer on 29.05.2017.
+ * Created by QAEngineer on 05.06.2017.
  */
-public class BaseClass {
-
+public class BasicClass {
     public static ThreadLocal <WebDriver> tlDriver = new ThreadLocal<>();
-    public WebDriver driver;
-    public WebDriverWait wait;
+    public static WebDriver driver;
+    public static WebDriverWait wait;
 
     @BeforeClass
     public void start(){
@@ -27,19 +24,25 @@ public class BaseClass {
             return;
         }
 
-
         ChromeOptions options = new ChromeOptions();
         options.addArguments("start-fullscreen");
+        options = new ChromeOptions();
         options.addArguments("start-maximized");
-        driver = new ChromeDriver(options);
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        tlDriver.set(driver);
+        System.out.println(((HasCapabilities) driver).getCapabilities());
         wait = new WebDriverWait(driver, 10);
+
+//        Runtime.getRuntime().addShutdownHook(
+//                new Thread(() -> {driver.quit(); driver = null;}));
 
 
     }
 
     @AfterClass
-    public void end(){
+    public static void end(){
         driver.quit();
         driver = null;
     }
-    }
+}
--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -2,10 +2,16 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -17,8 +23,14 @@ public class FirstClass {
 
     @Before
     public void start(){
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//        driver = new ChromeDriver();
+//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//        wait = new WebDriverWait(driver, 10);
+        DesiredCapabilities caps = new DesiredCapabilities();
+        driver = new FirefoxDriver(
+                new FirefoxBinary(new File("C:\\Program Files (x86)\\Nightly\\firefox.exe")),
+                new FirefoxProfile(),caps);
+        System.out.println(((HasCapabilities) driver).getCapabilities());
         wait = new WebDriverWait(driver, 10);
     }
 
